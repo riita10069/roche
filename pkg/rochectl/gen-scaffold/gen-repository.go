49f5447e30ae7fbb3231feea98045231fec5c795
ryota yamada <ryota@ryota-yamadanoMacBook-Pro.local>
2021-06-16 23:13:37 +0900

    u to r repo.Update method receiver≈

--- pkg/rochectl/gen-scaffold/gen-repository.go
+++ pkg/rochectl/gen-scaffold/gen-repository.go
@@ -129,7 +129,7 @@ func GenerateRepository(name string, targetStruct *ast.StructType, sqlMap map[st
 	)
 
 	// Update
-	infraRepoFile.Func().Params(Id("u").Id(repository)).Id("Update").Params(Id("id").Int64(), Id("e").Id("*entity.Enterprise")).Params(Id("*entity."+name), Error()).Block(
+	infraRepoFile.Func().Params(Id("r").Id(repository)).Id("Update").Params(Id("id").Int64(), Id("e").Id("*entity.Enterprise")).Params(Id("*entity."+name), Error()).Block(
 		List(Id("stmt"), Id("err")).Op(":=").Id("r").Dot("DB").Dot("Prepare").Call(Id(UpdateSQL(name, sqlMap))),
 		If(
 			Err().Op("!=").Nil(),
